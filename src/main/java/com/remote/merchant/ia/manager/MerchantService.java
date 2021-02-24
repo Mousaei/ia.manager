140296c27fa4b111ce8f3471cdfe0c6a930c5b93
Mousaei <vahidmfr@yahoo.fr>
2021-02-24 17:57:58 +0100

    Commit Mercredi 24 Fev 2021
    Commit du Mercredi 24 Fev 2021

--- src/main/java/com/remote/merchant/ia/manager/MerchantService.java
+++ src/main/java/com/remote/merchant/ia/manager/MerchantService.java
@@ -27,11 +27,20 @@ public class MerchantService {
 	           "<html>\n" 
 	        		+ "<header><title>Welcome</title></header>\n" 
 	        		+ "<body>\n" + "Hello world,\n" + " "
-	          		+ "\n" + "Nous allons acheter 4 kilos d'organes.\n" + ""
-	          		+ "\n" + "Nous allons acheter 4 kilos de fruits et légumes (pommes, oranges, ...). Notre point de départ est le point A. \n Nous passerons par le Point B et C avant d'arriver au supermarché: Soit le point D.\n" + "" + "<br/>"
-	          		+ "\n" + "Nous pouvons livrer 3 kilos de fruits ou légumes sur notre trajet de retour. Vous devez vous inscrire sur notre site:\n" + "" + "<br/>"
-	          		+ "\n" + " 1- Pour Acheter ou vendre des Fruits et Légumes si vous êtes sur notre trajet de retour. 2- Pour Acheter ou vendre des Fruits et Légumes si vous êtes sur le trajet de retour d une des personnes inscrites sur notre plateforme.\n" + "" + "<br/>"
+	          		+ "\n" + "Nous allons acheter 4 kilos d'oranges.\n" + ""
+	          		+  "<p></p>\n"
+	          		+ "\n" + "Nous allons acheter 4 kilos de fruits et légumes (pommes, oranges, ...)."
+	          		+  "<p></p>\n" +"Notre point de départ est le point A. \n" 
+	          		+  "Nous passerons par le Point B et C avant d'arriver au supermarché:\n"
+	          		+  "Soit le point D.\n" + "" + "<br/>"
+	          		+  "<p></p>\n"
+	          		+ "\n" + "Nous pouvons livrer 3 kilos de fruits ou légumes sur notre trajet de retour. "
+	          		+  "<p></p>\n"	+ "Vous devez vous inscrire sur notre site:\n" + "" + "<br/>"
+	          		+ "\n" + " 1- Pour acheter ou vendre des Fruits et Légumes si vous êtes sur notre trajet de retour. "
+	          		+  "<p></p>\n" + "2- Pour acheter ou vendre des Fruits et Légumes si vous êtes sur le trajet de retour d une des personnes inscrites sur notre plateforme.\n" + "" + "<br/>"
+	          		+  "<p></p>\n"
 	          	    + "<a href=" + "http://nutrition.object-decision.com>" + "This is my first link</a>" 
+	          	    +  "<p></p>\n"
 
  
 					
@@ -39,7 +48,37 @@ public class MerchantService {
 					+ "<h2>HTML Forms</h2>"
 					+  "<form action='/merchant'>"
 					+  "<fieldset>"
-					+  "<legend>Champs simples</legend>"
+					+  "<legend>Choose your features</legend>"
+					+  "<p></p>\n"
+					+ "\n"
+					+ "<div>\n"
+					+ "  <input type=\"checkbox\" id=\"FruitsLegumes\" name=\"FruitsLegumes\"\n"
+					+ "         checked>\n"
+					+ "  <label for=\"FruitsLegumes\">Recherche des Fruits et Legumes</label>\n"
+					+ "</div>\n"
+					
+					
+					+ "\n"
+					+ "<div>\n"
+					+ "  <input type=\"checkbox\" id=\"Personnes\" name=\"Personnes\"\n"
+					+ "         checked>\n"
+					+ "  <label for=\"Personnes\">Recherche des Personnes</label>\n"
+					+ "</div>\n"
+					+ "<div>\n"
+					+ "  <input type=\"checkbox\" id=\"Trajets\" name=\"Trajets\">\n"
+					+ "  <label for=\"Trajets\">Recherche des Trajets</label>\n"
+					+ "</div>\n"
+					+ "\n"
+					
+					+ "<div>\n"
+					+ "  <input type=\"checkbox\" id=\"InscriptionAbonnement\" name=\"Abonnement\"\n"
+					+ "  <label for=\"InscriptionAbonnement\">Inscription et Abonnement</label>\n"
+					+ "</div>\n"
+
+					+  "<p></p>\n"
+					+  "<p></p>\n"
+
+					
 					+  "<label for='fname'>First name:</label><br>"
 					+  "<input type='text' id='fname' name='fname' value='John'><br> "
 						
